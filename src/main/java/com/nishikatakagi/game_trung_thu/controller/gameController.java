a1489dd1fe71d16a1340e5057d0110e8bd382186
nishikatakagi <quocviet242003@gmail.com>
2024-09-15 20:10:46 +0700

    feat: some some html and there css page game

--- src/main/java/com/nishikatakagi/game_trung_thu/controller/gameController.java
+++ src/main/java/com/nishikatakagi/game_trung_thu/controller/gameController.java
@@ -1,10 +1,19 @@
 package com.nishikatakagi.game_trung_thu.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-
+@RequestMapping("")
 public class gameController {
+    @GetMapping("")
+    public String showIndex() {
+        return "/index.html";
+    }
 
+    @GetMapping("/khobau")
+    public String showGame1() {
+        return "/part1.html";
+    }
 }
---
+++ src/main/resources/static/js/part1.js
@@ -0,0 +1,36 @@
+// Get the suggest button and popup container elements
+const suggestBtn = document.getElementById('suggest-btn');
+const popupContainer = document.getElementById('popup-container');
+const popupText = document.getElementById('popup-text');
+const divbutton = document.getElementById("game_btn");
+const maxButton = 899;
+// Add an event listener to the suggest button
+suggestBtn.addEventListener('click', () => {
+  // Add additional text or attributes
+  popupText.textContent = "Dòng thứ ... từ dưới lên. hihi";
+
+  // Show the popup container
+  popupContainer.style.display = 'block';
+});
+
+// Add an event listener to the popup container to close it when clicked outside
+popupContainer.addEventListener('click', (e) => {
+  if (e.target === popupContainer) {
+    // Hide the popup container
+    popupContainer.style.display = 'none';
+  }
+});
+
+function addButton(){
+  var button = document.createElement("button");
+  button.textContent="Enter this";
+  divbutton.appendChild(button);
+}
+for(var i =0 ;i<= maxButton;i++)
+{
+  addButton();
+}
+var op = 1;
+function popup(){
+
+}
